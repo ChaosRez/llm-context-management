68426fc234112688279a892562c51fc62793d1d3
Reza <r.malek@keemail.me>
2025-05-08 12:55:03 +0200

    alexandra script update
    # Conflicts:
    #	fred/alexandra-node-1.sh
    #	fred/alexandra-node-2.sh
    #	fred/alexandra.sh

--- fred/alexandra-node-2.sh
+++ fred/alexandra-node-2.sh
@@ -5,13 +5,13 @@ EDGE_IP_1=$EDGE_IP_1
 EDGE_IP_2=$EDGE_IP_2
 
     ./alexandra\
-    --address "$EDGE_IP_1:10000" \
+    --address "$EDGE_IP_2:10000" \
     --lighthouse :9001 \
     --ca-cert cert/ca.crt \
-    --alexandra-key cert/frededge1.key \
-    --clients-key cert/frededge1.key \
-    --alexandra-cert cert/frededge1.crt \
-    --clients-cert cert/frededge1.crt \
+    --alexandra-key cert/frededge2.key \
+    --clients-key cert/frededge2.key \
+    --alexandra-cert cert/frededge2.crt \
+    --clients-cert cert/frededge2.crt \
     --experimental
 
 set +x
